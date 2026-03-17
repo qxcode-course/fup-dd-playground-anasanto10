b8056aa83c64c9ecf447c2586d8403f8c830d527
anasanto10 <ana.santos1918@aluno.ce.gov.br>
2026-03-17 18:11:19 +0000

    alterações

---
+++ sandbox/00_rascunhos/ifelse/draft.go
@@ -0,0 +1,57 @@
+package main
+import "fmt"
+func main() {
+//    var dia string = "seg"
+//   if dia == "seg" || dia == "ter" || dia == "quar" {
+//       fmt.Println("aula")
+//   } else if dia == "sab" || dia == "dom" {
+//       fmt.Println("descanso")
+//   } else {
+//        fmt.Println("estudo")
+//    }
+
+
+   // fome := true
+   // dinheiro := true
+   // if fome && dinheiro {
+   //     fmt.Println("vou almoçar")
+   // } else if fome {
+   //     fmt.Println("vou ficar com fome")
+   // } else {
+   //     fmt.Println("não estou com fome")
+   // }
+
+
+    //   idade := 17
+    //   if idade < 18 {
+    //     fmt.Println("criança")
+    //   } else if idade < 65 {
+    //     fmt.Println("adulto")
+    //   } else {
+    //     fmt.Println("idoso")
+    //   }
+
+
+    // jog1:= "pedra"
+    // jog2 := "papel"
+    // if jog1 == jog2 {
+    //     fmt.Println("empate")
+    // } else if (jog1 == "pedra" && jog2 == "tesoura") || 
+    // (jog1 == "papel" && jog2 == "pedra") ||
+    //  (jog1 == "tesoura" && jog2 =="papel")
+    //  {  fmt.Println("jog1")
+    // } else {
+    //     fmt.Println("jog2")
+    // }
+
+
+// jog1 := 0
+// jog2 := 1
+// jog3 := 0
+// if jog1 == jog2 && jog2 == jog3 {
+//     fmt.Println("empate")
+// } else if jog2 == jog3{
+//     fmt.Println("jog1")
+// }
+
+}
