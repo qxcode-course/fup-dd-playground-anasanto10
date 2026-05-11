adacdac9a65d52a188a11ad7fdfe285f9d327559
anasanto10 <ana.santos1918@aluno.ce.gov.br>
2026-05-11 17:58:46 +0000

    vetores

--- fup/batraquios/src/go/draft.go
+++ fup/batraquios/src/go/draft.go
@@ -1,5 +1,21 @@
 package main
 import "fmt"
 func main() {
-    fmt.Println("Hello, World!")
+    var N int
+    fmt.Scan(&N)
+    vet1 := make([]int, N)
+
+    for i := 0; i < N; i++ {
+        fmt.Scan(&vet1[i])
+    }
+
+    var M int
+    fmt.Scan(&M)
+    vet2 := make([]int, M)
+
+    for i := 0; i < M; i++ {
+        fmt.Scan(&vet2[i])
+    }
+
+
 }
--- fup/dieta/src/go/draft.go
+++ fup/dieta/src/go/draft.go
@@ -3,6 +3,18 @@ import "fmt"
 func main() {
     var dias int
     fmt.Scan(&dias)
+    calorias := make([]int, dias)
 
-    
+    soma := 0
+
+    for i := 0; i < dias; i++ {
+        fmt.Scan(&calorias[i])
+
+        soma += calorias[i]
+    }
+
+    media := float64(soma) / float64(dias)
+    fmt.Printf("%.1f\n", media)
 }
+
+
